07313c4bb92066649b904cd10ca0b050ef2bb416
Blue Sands STEM Labs <developer@bluesandsacademy.org>
2025-08-07 17:30:04 +0100

    Updated Program.cs(CORS)
    Added webapp URL to allow CORS

--- BlueSandsLMS.Api/Program.cs
+++ BlueSandsLMS.Api/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://www.bluesandstemlabs.com")
+        policy.WithOrigins("http://localhost:3000", "https://www.bluesandstemlabs.com", "https://app.bluesandstemlabs.com")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
